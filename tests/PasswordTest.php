d9af206c53480719f6ae3df20884639ab0a099f7
Avrelle <anto.delahaye@gmail.com>
2023-10-04 14:03:23 +0200

    test unitaire mp

---
+++ tests/PasswordTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class PasswordValidationTest extends TestCase
+{
+    public function testValidePassword()
+    {
+        $validator = Validation::createValidator();
+        $password = 'Toto';
+
+        $constraint = new Regex([
+            'pattern' => '/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
+        ]);
+
+        $violations = $validator->validate($password, $constraint);
+
+        $this->assertCount(0, $violations);
+    }
+
+    public function testInvalidPassword()
+    {
+        $validator = Validation::createValidator();
+        $password = 'tata';
+
+        $constraint = new Regex([
+            'pattern' => '/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
+        ]);
+
+        $violations = $validator->validate($password, $constraint);
+
+        $this->assertCount(1, $violations);
+    }
+}
